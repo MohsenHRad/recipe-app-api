a5eb0e901e6de36cfbb14e0abd6596f6aee02828
Mohsen <mohsen.h.rad.82@gmail.com>
2025-01-16 10:57:54 +0330

    Revert "login & register"
    This reverts commit ee68691da72418a6716880b0944635d77b7c0ee1.

--- blog_project/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='starting-page'),
-    path('posts', views.posts, name='posts-page'),
-    path('posts/<slug:slug>', views.single_post, name='posts-detail-page')
-]
