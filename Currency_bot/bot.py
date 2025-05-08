73d4817716093adf7b0f2e3349640a6e2b4944e3
Mohsen <mohsen.h.rad.82@gmail.com>
2025-05-08 18:21:48 +0330

    Added Github Actions

--- Currency_bot/bot.py
+++ Currency_bot/bot.py
@@ -1,8 +1,14 @@
+import sqlite3
+
+import asyncio
+import nest_asyncio
+nest_asyncio.apply()
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot
 from telegram.ext import CommandHandler, Application, CallbackContext, CallbackQueryHandler
 
 from chart import draw_price_chart
-from scraper import get_all_prices, get_last_5_days_prices
+from scraper import get_all_prices, get_last_5_days_prices, save_prices_to_database
 
 # python-telegram-bot → برای ساخت و مدیریت ربات تلگرام
 # requests → برای گرفتن اطلاعات قیمت از API
@@ -12,6 +18,7 @@ TOKEN = '7981755421:AAEF1r1EjZqJXLaTMasZEyAVVFNe1FfXRw8'
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("سلام! من ربات قیمت ارز و طلا هستم. 🤖")
+    await update.message.reply_text('/help - برای راهنمایی را ارسال کنید')
 
 
 async def help_command(update: Update, context: CallbackContext) -> None:
@@ -31,6 +38,7 @@ async def prices_command(update: Update, context: CallbackContext):
         [InlineKeyboardButton("🪙 طلا", callback_data='gold')],
         [InlineKeyboardButton("📊 همه", callback_data='all')],
         [InlineKeyboardButton("📈 نمودار تغییرات", callback_data='chart')],
+        # [InlineKeyboardButton("⌛ آخرین تغیرات ", callback_data='last_update')],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -42,12 +50,17 @@ async def button_clicked(update: Update, context: CallbackContext) -> None:
     await query.answer()
 
     prices = get_all_prices()
+    save_prices_to_database()
+    time_updated = get_last_updates()
+    dict_time_updated = {name: time for name, _, time in time_updated}
+    # print(time_updated)
 
     choice = query.data
 
     if choice == 'currency':
         filtered_price = {key: value for key, value in prices.items() if
                           key in ["یورو", "پوند انگلیس", "دلار", "دلار کانادا"]}
+
     elif choice == 'gold':
         filtered_price = {key: value for key, value in prices.items() if key in ["طلا 18 عیار",
                                                                                  "طلا 24 عیار",
@@ -59,13 +72,15 @@ async def button_clicked(update: Update, context: CallbackContext) -> None:
         last_5_prices = get_last_5_days_prices()
         if last_5_prices:
             draw_price_chart(list(last_5_prices.keys()), list(last_5_prices.values()))
-            await draw_chart_command(context.bot.token,query.message.chat.id)
+            await draw_chart_command(context.bot.token, query.message.chat.id)
     else:
         await query.message.reply_text("❌ خطا در دریافت داده‌ها برای نمودار.")
+        return
         # filtered_price = prices
-
-    message = "\n___________________________\n".join(
-        f"{name} : {price} ریال " for name, price in filtered_price.items())
+    message = message = "\n___________________________________________________________\n".join(
+        f"{name} : {price} ریال\n⏰ آخرین بروزرسانی: {dict_time_updated.get(name, 'نامشخص')}"
+        for name, price in filtered_price.items()
+    )
     await update.callback_query.message.reply_text(message)
 
 
@@ -87,16 +102,44 @@ async def draw_chart_command(bot_token, chat_id):
         await bot.send_photo(chat_id=chat_id, photo=photo, caption="📊 نمودار تغییرات قیمت")
 
 
-def main():
+def get_last_updates():
+    conn = sqlite3.connect('prices.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT name, price , last_update FROM prices')
+    prices = cursor.fetchall()
+    conn.close()
+
+    return prices
+
+
+def start_schdule():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(get_all_prices, 'interval', minutes=30)
+    scheduler.start()
+    return scheduler
+
+
+async def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('prices', prices_command))
     app.add_handler(CallbackQueryHandler(button_clicked))
+
+
+    # scheduler = AsyncIOScheduler()
+    # scheduler.add_job(get_all_prices, 'interval', minutes=4)
+    # scheduler.start()
+    start_schdule()
+
+
     print("ربات راه‌اندازی شد... 🚀")
-    app.run_polling()
+    await app.run_polling(close_loop=False)
 
 
 if __name__ == '__main__':
-    main()
+    # print(get_last_updates())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
--- Currency_bot/scraper.py
+++ Currency_bot/scraper.py
@@ -1,8 +1,9 @@
 import datetime
+import sqlite3
+import time
 
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
 
 url_dollar = 'https://www.tgju.org/profile/price_dollar_rl'
 
@@ -11,6 +12,7 @@ urls = {
     'دلار کانادا': 'https://www.tgju.org/profile/price_dollar_rl',
     'یورو': 'https://www.tgju.org/profile/price_eur',
     'پوند انگلیس': 'https://www.tgju.org/profile/price_gbp',
+    'دینار عراق': 'https://www.tgju.org/profile/price_iqd',
     'طلا 18 عیار': 'https://www.tgju.org/profile/geram18',
     'طلا 24 عیار': 'https://www.tgju.org/profile/geram24',
     'مثقال طلا': 'https://www.tgju.org/profile/mesghal',
@@ -18,6 +20,7 @@ urls = {
     'سکه بهار آزادی': 'https://www.tgju.org/profile/sekeb',
 
 }
+
 
 def get_dollar_price():
     response = requests.get(urls['دلار'])
@@ -62,25 +65,26 @@ def get_dates_prices():
 
     # if response.status_code !=200:
     #     return "خطا در دریافت اطلاعات ❌"
-    soup = BeautifulSoup(response.text,'html.parser') if response.status_code ==200 else None
+    soup = BeautifulSoup(response.text, 'html.parser') if response.status_code == 200 else None
+
 
 def get_last_5_days_prices():
     url = 'https://www.tgju.org/profile/geram18/history'
 
     try:
         response = requests.get(url)
-        table = BeautifulSoup(response.text, 'html.parser') if response.status_code == 200 else None # پیدا کردن جدول
-        rows = table.find('table').find_all('tr',limit=6) # گرفتن 6 سطر آخر
+        table = BeautifulSoup(response.text, 'html.parser') if response.status_code == 200 else None  # پیدا کردن جدول
+        rows = table.find('table').find_all('tr', limit=6)  # گرفتن 6 سطر آخر
 
         prices = {}
 
         for row in rows:
             columns = row.find_all('td')
 
-            if len(columns) <4: # بررسی اینکه حداقل ۴ ستون داشته باشد
+            if len(columns) < 4:  # بررسی اینکه حداقل ۴ ستون داشته باشد
                 continue
 
-            final_price = columns[3].text.strip().replace(',','')
+            final_price = columns[3].text.strip().replace(',', '')
             date = columns[-1].text.strip()
 
             try:
@@ -92,18 +96,61 @@ def get_last_5_days_prices():
         print(f"❌ خطا در دریافت داده‌ها: {e}")
         return {}
 
+
 pricess = get_last_5_days_prices()
 print(pricess)
 
-def save_prices_to_database(prices:dict):
+
+def save_prices_to_database():
+    prices = get_all_prices()
     conn = sqlite3.connect('prices.db')
     cursor = conn.cursor()
 
+    for name, price in prices.items():
+        old_prices , old_timestamp = get_last_price(name)
+        if str(price) != str(old_prices):
+        # print(price)
+        # print(cached_prices)
+        # if str(price) != str(cached_prices)
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            timestamp = old_timestamp
 
-    for name,price in prices.items():
-        timestamp = datetime.now()
+
+        cursor.execute('''
+            INSERT INTO prices(name,price,last_update)
+            VALUES (?,?,?)
+            ON CONFLICT (name) DO UPDATE SET 
+            price = EXCLUDED.price,
+            last_update = EXCLUDED.last_update
+        
+        ''', (name, price, timestamp))
+
+        conn.commit()
+    conn.close()
+
+def get_last_price(name):
+    conn = sqlite3.connect('prices.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT price , last_update FROM prices WHERE name = ?',(name,))
+    result = cursor.fetchone()
+
+    conn.close()
+    return result if result else (None, None)
+# def update_prices_periodically():
+#     price_update =
+#     while True:
+#         prices = get_all_prices()
+#         save_prices_to_database(prices)
+#         print(f'prices updated at {datetime.now()}')
+#         time.sleep(1800)
 
 
 if __name__ == "__main__":
     print(get_all_prices())
+    # for name,price in pricess.items():
+    #     print(get_last_price(name))
+    # print(save_prices_to_database(pricess))
+    # update_prices_periodically()
     # print(get_dates_prices())
